fbadc561ca20c03e8d1b5178991b2faaf847fcc6
yuyaapril <154661752+yuyaapril@users.noreply.github.com>
2025-11-01 18:04:22 +0630

    create repository

---
+++ src/main/java/b/b/book/repos/UserRepo.java
@@ -0,0 +1,13 @@
+package b.b.book.repos;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import b.b.book.models.AppUser;
+
+@Repository //@Autowired
+public interface UserRepo extends JpaRepository<AppUser, Long> {
+  Optional<AppUser> findByName(String username);
+}
