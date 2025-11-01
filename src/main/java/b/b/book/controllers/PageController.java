5d72cbd08bf0e7b7010c872bd3fc353e5945f108
yuyaapril <154661752+yuyaapril@users.noreply.github.com>
2025-11-01 21:27:33 +0630

    user register

--- src/main/java/b/b/book/controllers/PageController.java
+++ src/main/java/b/b/book/controllers/PageController.java
@@ -1,12 +1,27 @@
 package b.b.book.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import b.b.book.models.AppUser;
+import b.b.book.repos.UserRepo;
+
 
 
 
 @Controller
 public class PageController {
+
+  @Autowired
+  private UserRepo userRepo;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+  
   @GetMapping("/login")
   public String login() {
     return "login";
@@ -20,4 +35,23 @@ public class PageController {
      return "home";
    }
 
+   @PostMapping("/register")
+   public String createUser(
+                            @RequestParam String username,
+                            @RequestParam String email,
+                            @RequestParam String password
+   ) {
+     System.out.println("Username : " + username);
+     System.out.println("Email : " + email);
+     System.out.println("Password : " + password);
+
+     AppUser user = new AppUser();
+     user.setUsername(username);
+     user.setEmail(email);
+     user.setPassword(password);
+
+     userRepo.save(user);
+
+     return "redirect:/login";
+   } 
 }
--- src/main/java/b/b/book/security/SecurityConfig.java
+++ src/main/java/b/b/book/security/SecurityConfig.java
@@ -1,18 +1,51 @@
 package b.b.book.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import b.b.book.services.UserService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor //SecurityConfig အလုပ်လုပ်တာနဲ့userService ကို ConstructorကနေInjectionလုပ်ပေးလိမ့်မယ်
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  private final UserService userService;
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return userService;
+  }
+  //daoAuthenticationProvider သုံးပြီးdaoProvider တစ်ခုဆောက် userService ကိုခေါ်သုံးပြီးအလုပ်လုပ်မယ်
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+    daoProvider.setUserDetailsService(userService);
+    daoProvider.setPasswordEncoder(passwordEncoder());
+    return daoProvider;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
+        .csrf(AbstractHttpConfigurer:: disable)
         .formLogin(httpForm -> {
           httpForm.loginPage("/login").permitAll();
         })
