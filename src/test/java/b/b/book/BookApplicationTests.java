d87d35bb5b7f8f578d7f455440095842aa79bb07
yuyaapril <154661752+yuyaapril@users.noreply.github.com>
2025-11-01 17:14:43 +0630

    first commit

---
+++ src/test/java/b/b/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package b.b.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
